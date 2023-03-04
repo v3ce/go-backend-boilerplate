33023c840d05fa51b2ac43fdfa5850b9fdbff855
Peng-Yu Chen <walkccray@gmail.com>
2023-03-03 20:21:47 -0500

    07 - Implement account balance and handle deadlock

--- db/query/account.sql
+++ db/query/account.sql
@@ -9,6 +9,13 @@ FROM accounts
 WHERE id = $1
 LIMIT 1;
 
+-- name: GetAccountForUpdate :one
+SELECT *
+FROM accounts
+WHERE id = $1
+LIMIT 1 FOR NO KEY
+UPDATE;
+
 -- name: ListAccounts :many
 SELECT *
 FROM accounts
@@ -18,6 +25,12 @@ LIMIT $1 OFFSET $2;
 -- name: UpdateAccount :one
 UPDATE accounts
 SET balance = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + sqlc.arg(amount)
 WHERE id = $1
 RETURNING *;
 
--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -33,6 +33,8 @@ func TestTransferTx(t *testing.T) {
 		}()
 	}
 
+	existed := make(map[int]bool)
+
 	for i := 0; i < n; i++ {
 		err := <-errs
 		require.NoError(t, err)
@@ -64,8 +66,35 @@ func TestTransferTx(t *testing.T) {
 		require.NotZero(t, toEntry.ID)
 		require.NotZero(t, toEntry.CreatedAt)
 
-		// TODO: Check accounts.
+		// Check accounts.
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
 
-		// TODO: Check accounts' balance.
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+
+		// Check accounts' balance.
+		diff1 := account1.Balance - fromAccount.Balance
+		diff2 := toAccount.Balance - account2.Balance
+		require.Equal(t, diff1, diff2)
+		require.True(t, diff1 > 0)
+		require.True(t, diff1%amount == 0)
+
+		k := int(diff1 / amount)
+		require.True(t, k >= 1 && k <= n)
+		require.NotContains(t, existed, k)
+		existed[k] = true
 	}
+
+	// Check the final updated balance.
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
 }
